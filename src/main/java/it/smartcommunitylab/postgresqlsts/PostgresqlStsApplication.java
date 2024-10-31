93714e7255cee9b6637637cff771e96b35eeb55a
Matteo Saloni <matteo.saloni@gmail.com>
2024-10-31 14:07:49 +0100

    chore: init

---
+++ src/main/java/it/smartcommunitylab/postgresqlsts/PostgresqlStsApplication.java
@@ -0,0 +1,13 @@
+package it.smartcommunitylab.postgresqlsts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlStsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/smartcommunitylab/postgresqlsts/PostgresqlStsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.smartcommunitylab.postgresqlsts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
